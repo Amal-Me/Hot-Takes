6550a82f755157ac0ca26eaf4e07047732531602
amel <amel.lys@hotmail.fr>
2021-11-25 20:51:12 +0100

    ajout fichier

---
+++ backend/middleware/authCreator.js
@@ -0,0 +1,12 @@
+const Sauce = require('../models/Sauce');
+
+module.exports = (req, res, next) => {
+   // on cherche la sauce pour récupérer l id du créateur
+    Sauce.findOne({ _id: req.params.id})
+    .then(sauce => { 
+    //comparaison id du créateur avec celui extrait du token
+    if (sauce.userId !== req.token.userId) {
+        throw 'Utilisateur non valide';
+      } else { next();}})
+    .catch(e => res.status(401).json({message: e}))
+}
